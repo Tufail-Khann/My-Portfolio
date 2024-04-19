885f0020db65e326f6b8628feff3f9d5b4f0ac72
Tufail-Khann <158440066+Tufail-Khann@users.noreply.github.com>
2024-04-19 19:13:59 +0530

    javascript
    js file

---
+++ java.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector(".button");
+let body = document.querySelector("body")
+
+// btn.addEventListener("click", ()=>{
+//     Aside.classList.add(".side");
+//     console.log("hiss")
+// })
+
+btn.addEventListener("click", ()=>{
+    body.classList.add(".side");
+});
